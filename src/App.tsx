fb0209c4743a22be0395a2ca82d4adfff279d265
Nguyen <huunguyen199999@gmail.com>
2024-03-04 15:04:15 +0700

    Setup React project with webpack

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from 'react'
+import './assets/styles/app.css'
+import './assets/styles/app.scss'
+
+const App: FunctionComponent = () => {
+  return <div className='text-3xl font-bold underline'>Hello, World!</div>
+}
+
+export default App
